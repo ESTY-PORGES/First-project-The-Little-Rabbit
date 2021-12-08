32114b938f6889c28a621ec9f739b13aeebc8dcf
EttyBlack <6166887@gmail.com>
2021-12-08 10:55:52 +0200

    Add Menu, Bonus, Nikud

---
+++ Assets/Prototype/Main/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Animator contentPanel;
+    public void onPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ToggleMenu()
+    {
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
+    }
+}
